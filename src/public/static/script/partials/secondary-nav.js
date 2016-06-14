75361a50dd70b1cec7d8fe79e134404f764a6198
Daljit Singh <d4lj1t@hotmaill.com>
2016-06-14 16:11:45 +0100

    secondary nav menu showing content with slide and hiding as should

--- src/public/static/script/partials/secondary-nav.js
+++ src/public/static/script/partials/secondary-nav.js
@@ -25,25 +25,50 @@ module.exports = function LogIn() {
          container.css('height', clientHeight);
          });*/
 
-        $('.js-menu-heading').mouseover(function () {
-                $(this).addClass('trigger').find('.js-menu-heading-content').stop().slideDown();
+    /*    $('.js-menu-heading').mouseover(function () {
+                /!*$(this).addClass('trigger').find('.js-menu-heading-content').stop().slideDown();*!/
+                $(this).addClass('trigger').find('.js-menu-content-wrapper').show().find('.js-menu-heading-content').stop().slideDown();
             }
         );
 
         $('.js-menu-heading').mouseleave(function () {
-                $(this).find('.js-menu-heading-content').stop().slideUp(function(){
-                    $(this).closest('.js-menu-heading').removeClass('trigger');
-                });
+                /!*  $(this).find('.js-menu-heading-content').stop().slideUp(function(){
+                 $(this).hide().closest('.js-menu-heading').removeClass('trigger');
+                 });*!/
+
+                $(this).find('.js-menu-heading-content').stop().slideUp().find('.js-menu-content-wrapper').hide().closest('.js-menu-heading').removeClass('trigger');
+
+                /!*$(this).hide().closest('.js-menu-heading').removeClass('trigger');*!/
+                /!*});*!/
 
 
             }
+        );*/
+
+        /*    $('.js-menu-heading').mouseout(function(){
+         console.log('hello');
+
+         $(this).find.('.js-menu-heading-content').stop().slideUp();
+         });*/
+
+
+        $('.js-menu-heading').mouseover(function () {
+            $(this).addClass('trigger').find('.js-menu-heading-content').stop().slideDown();
+            }
         );
 
-    /*    $('.js-menu-heading').mouseout(function(){
-            console.log('hello');
+        $('.js-menu-heading').mouseleave(function () {
+                $(this).removeClass('trigger').find('.js-menu-heading-content').hide();
 
-            $(this).find.('.js-menu-heading-content').stop().slideUp();
-        });*/
+               /* $(this).find('.js-menu-heading-content').stop().slideUp(function(){
+                    $(this).closest('.js-menu-heading').removeClass('trigger');
+                });*/
+            }
+        );
+
+
+
+
     };
 
 
