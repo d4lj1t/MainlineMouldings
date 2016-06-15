736ff6c022e05acc47193fc4f5539644e0a89ad5
Daljit Singh <d4lj1t@hotmaill.com>
2016-06-15 09:50:20 +0100

    cleaned side-menu.js

--- src/public/static/script/partials/side-menu.js
+++ src/public/static/script/partials/side-menu.js
@@ -18,30 +18,34 @@ module.exports = function LogIn() {
     self.init = function () {
 
         var animationSpeed = 200,
-            container = $('.js-side-menu');
+            container = $('.js-side-menu'),
+            btnMenu = $('.js-btn-menu'),
+            btnTimes = $('.js-btn-times'),
+            clicked = ('clicked'),
+            sideMenuWidth = '250px';
 
         $(window).resize(function() {
             var clientHeight = $(window).height();
             container.css('height', clientHeight);
         });
 
-        $('.js-btn-menu').click(function () {
+        btnMenu.click(function () {
 
-                container.addClass('clicked').show().stop().animate({width: '250px'}, animationSpeed);
+                container.addClass(clicked).show().stop().animate({width: sideMenuWidth}, animationSpeed);
 
                 $(document).bind("mouseup touchend", function (e) {
 
                     if (!container.is(e.target) // if the target of the click isn't the container...
                         && container.has(e.target).length === 0 // ... nor a descendant of the container
-                        || $('.js-btn-times').is(e.target)) // ... if target is the side-menu cross icon
+                        || btnTimes.is(e.target)) // ... if target is the side-menu cross icon
                     {
                         /*container.animate({width: '0'});*/
-                        if (container.hasClass('clicked')) {
+                        if (container.hasClass(clicked)) {
 
                             container.stop().animate({
                                 width: '0'
                             }, animationSpeed, function () {
-                                $(this).removeClass('clicked').hide();
+                                $(this).removeClass(clicked).hide();
 
                             });
                         }
@@ -55,9 +59,6 @@ module.exports = function LogIn() {
     /*
      Internal
      ----------------------------------- */
-
-// Log in methods
-
 
     self.init();
 
