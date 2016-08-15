68b726566a98b88d752faa5b11f74ebb216b823f
Daljit Singh <d4lj1t@hotmaill.com>
2016-08-15 15:28:59 +0100

    image zoom with focus lens not looking bad on mobile

---
+++ src/public/static/script/plugins/pdp.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+/*    $("#img_01").elevateZoom();*/
+
+
+    $("#img_01").elevateZoom({
+        zoomType				: "lens",
+        lensShape : "round",
+        lensSize    : 200
+    });
+
+});
